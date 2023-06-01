71d8334df838334ac55cf20a738722c3f70a2dad
kate-cronin <97194571+kate-cronin@users.noreply.github.com>
2023-06-01 12:47:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+//PUZZLE 1: Replace the ??? so that the code below will add the number 5 to the array 'numbers'.
+
+// let numbers = [1, 2, 3, 4]
+
+// numbers.push(???);
+// console.log(numbers)
+
+
+// PUZZLE 2: Write the code that will log the length of the array veggies to the console. 
+
+// let veggies = ["broccoli", "carrots", "squash", "spinach", "collard greens", "cucumbers", "radishes", "zuchinni", "pumpkin", "tomatoes", "kale", "sprouts", "green beans", "green peas"]
+
+//Write your code here.
+
+//PUZZLE 3: Write the code that will add the vegetable "potato" to the array veggies. Now print the length of the new array to the console. 
+
+//Write your code here.
+
+
+//PUZZLE 4: Write a for of loop that logs each of the fruits in the fruits array below to the console. 
+
+//let fruits = ["apple", "orange", "banana"];
+
+//WRITE YOUR CODE HERE
+
+
+//EXCELLENT! Let's take the spice level up a notch now! You will use the array below (named 'bigList') for the next few puzzles. 
+
+let bigList = [81, 47, 66, 15, 11, 25, 45, 5, 99, 85, 58, 46, 21, 92, 38, 84, 66, 93, 65, 61, 7, 85, 2, 74, 66, 88, 65, 48, 58, 87, 41, 56, 17, 46, 26, 87, 41, 69, 48, 34, 69, 3, 46, 65, 67, 76, 46, 35, 68, 86];
+
+//PUZZLE 5: You will need to combine your knowledge of conditionals, arrays, and loops to solve this puzzle. Write the code that will log all of the numbers greater than 50 OR less than 30 in the array bigList to the console. 
+
+
+//WRITE YOUR CODE HERE
+
+
+//PUZZLE 6: Without counting the number of array elements in the array bigList, use the .length method to print the last number in the array to the console. 
+
+
+//WRITE YOUR CODE HERE
+
+
+// PUZZLE 7: Split the array 'bigList' (above) into 2 different lists — Numbers less than 50, and numbers greater than or equal to 50. Hint: you will need to declare two empty arrays (name them whatever you want) and use the .push method to PUSH the numbers into those new arrays depending on if they are less than 50 or greater then or equal to 50. 
+
+//WRITE YOUR CODE HERE 
+
+
+//PUZZLE 8: Without counting manually, write a code that will tell you how many numbers in the array bigList are greater than 25. There are several different ways you can solve this! 
+
+
+
+
+
+
+
+
+
+
+
+
